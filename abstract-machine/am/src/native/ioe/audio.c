fffb957bc6343f3fe9e4c87dab33be069b48bd95
ioyoi <i@ioyoi.me>
2025-10-16 00:48:56 +0800

    fix(abstract-machine): fix _GNU_SOURCE redefined

--- abstract-machine/am/src/native/ioe/audio.c
+++ abstract-machine/am/src/native/ioe/audio.c
@@ -1,4 +1,6 @@
+#ifndef _GNU_SOURCE
 #define _GNU_SOURCE
+#endif /* ifndef _GNU_SOURCE */
 #include <fcntl.h>
 #include <unistd.h>
 #include <klib.h>
--- abstract-machine/am/src/native/platform.c
+++ abstract-machine/am/src/native/platform.c
@@ -1,4 +1,6 @@
+#ifndef _GNU_SOURCE
 #define _GNU_SOURCE
+#endif /* ifndef _GNU_SOURCE */
 #include <sys/mman.h>
 #include <sys/auxv.h>
 #include <dlfcn.h>
